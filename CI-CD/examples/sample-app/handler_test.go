e47afb97180e6496f1e72dc8fb0d97bbb40a5583
r07627015 <r07627015@ntu.edu.tw>
2026-04-25 17:29:33 +0800

    test

--- CI-CD/examples/sample-app/handler_test.go
+++ CI-CD/examples/sample-app/handler_test.go
@@ -17,7 +17,7 @@ func TestHandleRoot(t *testing.T) {
 		t.Errorf("expected status %d, got %d", http.StatusOK, rec.Code)
 	}
 
-	expected := "Hello, RRRJASO  N"
+	expected := "Hello, RRRJAS O  N"
 	if rec.Body.String() != expected {
 		t.Errorf("expected body %q, got %q", expected, rec.Body.String())
 	}
